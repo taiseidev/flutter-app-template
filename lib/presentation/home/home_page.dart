49c181d106487d0329ba4558ae54001efa7da36b
onishi taisei <taisei.developer@gmail.com>
2024-01-24 01:01:19 +0900

    🚀 feat: 自作パッケージを追加

--- lib/presentation/home/home_page.dart
+++ lib/presentation/home/home_page.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app_template/extensions/async_value_extension.dart';
-import 'package:flutter_app_template/extensions/multi_async_value_extension.dart';
 import 'package:flutter_app_template/extensions/widget_ref_extension.dart';
 import 'package:flutter_app_template/presentation/home/home_controller.dart';
 import 'package:flutter_app_template/providers/home/get_sample_int_provider.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:multi_async_value/multi_async_value.dart';
 
 class HomePage extends ConsumerWidget {
   const HomePage({super.key});
@@ -24,12 +24,16 @@ class HomePage extends ConsumerWidget {
 
     return Scaffold(
       appBar: AppBar(
-        title: MultiAsyncValue.group2(asyncValue, asyncValue2)
-            .handleAsyncValue((value) {
-          final value1 = value.$1;
-          final value2 = value.$2;
-          return Text((value1 + value2).toString());
-        }),
+        title: MultiAsyncValue.group2(
+          asyncValue,
+          asyncValue2,
+        ).handleAsyncValue(
+          (value) {
+            final value1 = value.$1;
+            final value2 = value.$2;
+            return Text((value1 + value2).toString());
+          },
+        ),
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
