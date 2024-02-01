78a12219ee7aac34f01338e59723daa087473068
onishi taisei <taisei.developer@gmail.com>
2024-02-01 21:45:56 +0900

    🚀 feat: 画質チェックの機能追加

--- lib/presentation/image_sample/image_sample_page.dart
+++ lib/presentation/image_sample/image_sample_page.dart
@@ -13,93 +13,107 @@ class ImageSamplePage extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
-    final notCompressFile = useState<File?>(null);
-    final compressFile = useState<Uint8List?>(null);
+    final notCompressImage = useState<File?>(null);
+    final compressedImage = useState<Uint8List?>(null);
+    final notCompressImageSize = useState<double?>(null);
+    final compressImageSize = useState<double?>(null);
+    final imageQuality = useState<double>(1.0);
 
     return Scaffold(
       body: Center(
         child: SingleChildScrollView(
           child: Column(
             children: [
-              const Text('画像に関するサンプルをこの画面に表示する'),
+              const Text('画像に関する処理をまとめる画面'),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text('画質を変更'),
+                  const SizedBox(width: 8),
+                  Text((imageQuality.value * 100).floor().toString()),
+                ],
+              ),
+              Slider(
+                value: imageQuality.value,
+                onChanged: (value) {
+                  imageQuality.value = value;
+                },
+              ),
               ElevatedButton(
                 onPressed: () async {
+                  // カメラで画像を取得
                   final result =
                       await ImagePickerService.takePictureFromCamera();
 
                   if (result != null) {
+                    // 圧縮前の画像を追加
+                    notCompressImage.value = File(result.path);
+
+                    // バイトからメガバイトに変換
                     final size =
                         await ImagePickerService.getImageSizeInMB(result);
 
-                    notCompressFile.value = File(result.path);
+                    notCompressImageSize.value =
+                        double.tryParse(size.toStringAsFixed(2));
 
-                    debugPrint(ImagePickerService.imageSizeDebugMessage(size));
+                    // 画像を圧縮
+                    final byte = await ImageCompressService().compressWithFile(
+                      result.path,
+                      quality: (imageQuality.value * 100).floor(),
+                    );
+
+                    if (byte != null) {
+                      // 圧縮後の画像を追加
+                      compressedImage.value = byte;
+
+                      final compressedSize =
+                          await ImagePickerService.getImageSizeInMBForByte(
+                        byte,
+                      );
+
+                      compressImageSize.value =
+                          double.tryParse(compressedSize.toStringAsFixed(2));
+                    }
 
                     final filePath = result.path;
                     final ext = p.extension(filePath);
                     debugPrint('拡張子 $ext');
-
-                    final byte = await ImageCompressService()
-                        .compressWithFile(result.path, quality: 70);
-
-                    if (byte != null) {
-                      compressFile.value = byte;
-                    }
                   }
                 },
                 child: const Text('カメラで取得'),
               ),
-              ElevatedButton(
-                onPressed: () async {
-                  final result =
-                      await ImagePickerService.pickImageFromGallery();
-                  if (result != null) {
-                    final size =
-                        await ImagePickerService.getImageSizeInMB(result);
-
-                    notCompressFile.value = File(result.path);
-
-                    debugPrint(ImagePickerService.imageSizeDebugMessage(size));
-
-                    final filePath = result.path;
-                    final ext = p.extension(filePath);
-                    debugPrint('拡張子 $ext');
-
-                    final byte = await ImageCompressService()
-                        .compressWithFile(result.path);
-
-                    if (byte != null) {
-                      compressFile.value = byte;
-                      final compressFileSize =
-                          await ImagePickerService.getImageSizeInMBForByte(
-                        byte,
-                      );
-                      debugPrint(compressFileSize.toString());
-                      debugPrint(
-                        ImagePickerService.imageSizeDebugMessage(
-                          compressFileSize,
-                        ),
-                      );
-                    }
-                  }
-                },
-                child: const Text('ギャラリーから取得'),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text('圧縮前の画像'),
+                  const SizedBox(width: 8),
+                  Text('${notCompressImageSize.value} MB'),
+                ],
               ),
-              if (notCompressFile.value != null)
+              if (notCompressImage.value != null)
                 ClipRRect(
                   borderRadius: BorderRadius.circular(100),
                   child: Image.file(
-                    notCompressFile.value!,
+                    notCompressImage.value!,
                     width: 200,
                     height: 200,
                     fit: BoxFit.cover,
                   ),
                 ),
-              if (compressFile.value != null)
+              const SizedBox(height: 24),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text('圧縮済みの画像'),
+                  const SizedBox(width: 8),
+                  Text('${compressImageSize.value} MB'),
+                ],
+              ),
+              if (compressedImage.value != null)
                 ClipRRect(
                   borderRadius: BorderRadius.circular(100),
                   child: Image.memory(
-                    compressFile.value!,
+                    compressedImage.value!,
                     width: 200,
                     height: 200,
                     fit: BoxFit.cover,
